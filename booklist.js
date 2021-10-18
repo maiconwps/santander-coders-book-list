07eda5166bab9b8015ba250d3063e8d9d4732ca9
Alan-Patrik <alanpatrik.fragozo@gmail.com>
2021-10-18 19:10:26 -0300

    finished version of the challenge

--- booklist.js
+++ booklist.js
@@ -1,68 +1,111 @@
-class Book{
+class Book {
+  constructor(title, genre, author) {
+    (this.title = title),
+      (this.genre = genre),
+      (this.author = author),
+      (this.read = false),
+      (this.readDate = undefined);
+  }
 
-    constructor(title, genre, author){
-       this.title = title
-       this.genre = genre
-       this.author = author
-       this.read = false
-       this.readDate = null
-    }
+  markAsRead() {
+    this.read = true;
+    this.readDate = new Date(Date.now());
 
-    markIsRead(){
-        this.read = true
-        this.readDate = new Date(Date.now())
-    }
+    return this.read && this.readDate;
+  }
 }
 
-class BookList{
-    constructor(){
-        this.booklist = []
-        this.numberBookRead = 0
-        this.indexCurrentBook = undefined
-    }
+class BookLists {
+  constructor() {
+    this.bookList = [];
+    this.numBooksRead = 0;
+    this.numBooksUnread = 0;
+    this.refNextBookRead = undefined;
+    this.refLastBookRead = undefined;
+    this.refCurrentBookRead = undefined;
+    this.indexCurrentBook = undefined;
+  }
 
-    get currentBook(){
-        return this.booklist[this.indexCurrentBook]
-    }
+  get currentBook() {
+    this.refCurrentBookRead = this.bookList[this.indexCurrentBook];
+    return this.refCurrentBookRead;
+  }
 
-    get nextBook(){
-        return this.booklist[this.indexCurrentBook + 1]
-    }
+  get nextBook() {
+    this.refNextBookRead = this.bookList[this.indexCurrentBook + 1];
+    return this.refNextBookRead ? this.refNextBookRead : "No more books..";
+  }
 
-    get lastBook(){
-        return this.booklist[this.indexCurrentBook - 1]
-    }
-    
-    addBook(book){
-        this.booklist = [...this.booklist, book]
+  get lastBook() {
+    this.refLastBookRead = this.bookList[this.indexCurrentBook - 1];
+    return this.refLastBookRead
+      ? "This is your first book.."
+      : this.refLastBookRead;
+  }
 
-        if(this.booklist.length === 1){
-            this.indexCurrentBook = 0
-        }
-    }
+  listAll() {
+    return this.bookList;
+  }
 
-    finishCurrentBook(){
-        this.currentBook.markIsRead()
-        this.numberBookRead += 1
-        this.indexCurrentBook =+ 1
+  addBook(book) {
+    this.bookList = [...this.bookList, book];
+    this.numBooksUnread++;
+
+    if (this.bookList.length === 1) {
+      this.indexCurrentBook = 0;
     }
+  }
+
+  finishCurrentBook() {
+    this.currentBook?.markAsRead();
+    this.numBooksRead++;
+    this.numBooksUnread--;
+    this.indexCurrentBook++;
+  }
+
+  searchByGenre(genre) {
+    return this.bookList.filter((book) => book.genre === genre);
+  }
+
+  searchByAuthor(author) {
+    return this.bookList.filter((book) => book.author === author);
+  }
+
+  searchByTitle(title) {
+    return this.bookList.filter((book) => book.title === title);
+  }
+
+  listAllBooksRead() {
+    return this.bookList.filter((book) => book.read === true);
+  }
+
+  listAllUnreadBooks() {
+    return this.bookList.filter((book) => book.read === false);
+  }
 }
 
-const book1 = new Book("Teste", "terror", "Murilo")
-const book2 = new Book("Teste2", "comedeia", "lala")
-const book3 = new Book("Teste3", "ficcao", "alal")
-// book1.read = true
+let book1 = new Book("I'm afraid!", "Horror", "Giulia");
+let book2 = new Book("I'm in love!", "Romance", "Larissa");
+let book3 = new Book("Help me Giulia!", "Horror", "Giulia");
 
-const bookList = new BookList()
-bookList.addBook(book1)
-// console.log(bookList)
-bookList.addBook(book2)
-bookList.addBook(book3)
+let list = new BookLists();
 
+list.addBook(book1);
+list.finishCurrentBook();
+list.addBook(book2);
+list.finishCurrentBook();
+list.addBook(book3);
 
-bookList.finishCurrentBook()
-bookList.finishCurrentBook()
-console.log(bookList.currentBook)
-console.log(bookList.nextBook)
-console.log(bookList.lastBook)
+console.log(list.searchByGenre("Horror"));
 
+console.log(list.searchByAuthor("Giulia"));
+
+console.log(list.searchByTitle("I'm in love!"));
+
+console.log(list.listAllBooksRead());
+
+console.log(list.listAllUnreadBooks());
+
+console.log(list.listAll());
+
+console.log(list.lastBook);
